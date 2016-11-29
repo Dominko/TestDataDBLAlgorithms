5244bf203a739d469320dd4c6942212423dca2a7
Dominko <Dominik@grabarczyk.nl>
2016-11-29 10:56:16 +0100

    Almost finished preamble writing

--- src/testdata/TestData.java
+++ src/testdata/TestData.java
@@ -14,16 +14,23 @@ public class TestData {
     int max_time;
     int number_taxis;
     int max_passengers;
-    int number_nodes;               
+    int number_nodes;    
+    int training_period;
+    int total_time;
     
     public void output(){
-        System.out.println();
+        System.out.println(5 + number_nodes);
         System.out.println(alpha);
         System.out.println(max_time);
         System.out.println(number_taxis + " " + max_passengers);
         System.out.println(number_nodes);
-        System.out.println();
-        System.out.println();
+        for(int x = 0; x < number_nodes; x++){
+            System.out.println(
+            
+            
+            );
+        }
+        System.out.println(training_period + " " + total_time);
         System.out.println();
     }
     
