b2ff489447db9ee2413c25d5b4990a17ddb51244
Dominko <Dominik@grabarczyk.nl>
2016-11-29 10:39:15 +0100

    Just come variables

--- src/testdata/TestData.java
+++ src/testdata/TestData.java
@@ -10,10 +10,23 @@ package testdata;
  * @author s156035
  */
 public class TestData {
-
-    /**
-     * @param args the command line arguments
-     */
+    float alpha;
+    int max_time;
+    int number_taxis;
+    int max_passengers;
+    int number_nodes;               
+    
+    public void output(){
+        System.out.println();
+        System.out.println(alpha);
+        System.out.println(max_time);
+        System.out.println(number_taxis + " " + max_passengers);
+        System.out.println(number_nodes);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
